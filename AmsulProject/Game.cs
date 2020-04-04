d5d9286afdbc448b6353468d9730842abbc5ad73
KhanalAmsul <58516414+KhanalAmsul@users.noreply.github.com>
2020-04-03 21:14:39 -0500

    fixed shit

--- AmsulProject/Game.cs
+++ AmsulProject/Game.cs
@@ -12,9 +12,17 @@ namespace AmsulProject
 
         public Game()//constructor
         {
-            board[0] = new char[3];
-            board[1] = new char[3];
-            board[2] = new char[3];
+            for (int y = 0; y <= 2; y = y + 1)
+            {
+                board[y] = new char[3];
+                for (int x = 0; x <= 2; x = x + 1)
+                {
+                    board[y][x] = '.';
+
+                }
+                /*board[1] = new char[3];
+                board[2] = new char[3];*/
+            }
         }
 
 
@@ -39,32 +47,66 @@ namespace AmsulProject
 
         public void AcceptPlayerInput()
         {
+           
             Console.WriteLine("Type Board Coords Like 11");
             string input = Console.ReadLine();
             if (input.Length == 2)
             {
-                char row = input[0];
-                int rowPos = Int32.Parse(row.ToString());
-                char col = input[1];
-                int colPos = Int32.Parse(col.ToString());
+                try
+                {
+                    char col = input[0];
+                    int colPos = Int32.Parse(col.ToString());
+                    char row = input[1];
+                    int rowPos = Int32.Parse(row.ToString());
+                
 
-                if(itIsXsTurn)
-                    board[rowPos][colPos] = 'x';
+                if (colPos <0 || colPos > board.Length-1 || rowPos < 0 || rowPos > board.Length-1)
+                {
+                    /*Console.WriteLine(colPos);
+                    Console.WriteLine(rowPos);*/
+                    Console.WriteLine("A tic tac toe board is 3 by 3 you idiot.");
+                }
                 else
-                    board[rowPos][colPos] = 'o';
+                {
 
+                    if (board[rowPos][colPos] != '.')
+                    {
+                        Console.WriteLine("There is already someone that is there.");
+                    }
+                    else
+                    {
+                        if (itIsXsTurn)
+
+                            board[rowPos][colPos] = 'x';
+                        else
+                            board[rowPos][colPos] = 'o';
+
+                        itIsXsTurn = !itIsXsTurn;
+                    }
+                }
+                }
+                catch
+                {
+                    Console.WriteLine("Yea Buddy, learn how to type a 2 digit number, idiot.");
+                }
+            }
+            else
+            {
+
+                Console.WriteLine("What the fuck are you even doing?");
             }
 
 
 
-        }
+        }//acceptPlayerInput end
 
         public void Run()
         {
             while(true) {
-                AcceptPlayerInput();
                 PrintTheBoard();
-                itIsXsTurn = !itIsXsTurn;
+                AcceptPlayerInput();
+                
+                //itIsXsTurn = !itIsXsTurn;
             }
         }
 
